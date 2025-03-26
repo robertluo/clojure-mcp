8c3c800e58616c839563e3dbdcfe9ba2de6e9d4f
Bruce Hauman <bhauman@gmail.com>
2025-03-26 16:58:38 -0600

    got it MVP working

--- src/clojure_mcp/core.clj
+++ src/clojure_mcp/core.clj
@@ -1,41 +1,100 @@
 (ns clojure-mcp.core
-  (:gen-class)
+  (:require [clojure.data.json :as json])
+  #_(:gen-class)
   (:import [io.modelcontextprotocol.server.transport StdioServerTransportProvider]
            [io.modelcontextprotocol.server McpServer McpServerFeatures
             McpServerFeatures$SyncToolSpecification]
            [io.modelcontextprotocol.spec
             McpSchema$ServerCapabilities
             McpSchema$Tool
-            McpSchema$CallToolResult]
-           [com.fasterxml.jackson.databind ObjectMapper]
-           [java.util.function BiFunction]))
+            McpSchema$CallToolResult
+            McpSchema$TextContent ]
+           [com.fasterxml.jackson.databind ObjectMapper]))
 
-(def hello-schema "{\"type\": \"object\"}")
+;; {
+;;       name: "calculate_sum",
+;;       description: "Add two numbers together",
+;;       inputSchema: {
+;;         type: "object",
+;;         properties: {
+;;           a: { type: "number" },
+;;           b: { type: "number" }
+;;         },
+;;         required: ["a", "b"]
+;;       }
+;;     }
+
+(defn capture-output [k]
+  (let [out-atom (atom "")
+        err-atom (atom "")
+        res (atom nil)]
+    (binding [*out* (java.io.StringWriter.)
+              *err* (java.io.StringWriter.)]
+              (reset! res (k))
+              (reset! out-atom (str *out*))
+              (reset! err-atom (str *err*)))
+    {:result @res :out @out-atom :err @err-atom}))
+
+;; just a mock eval tool
+(defn eval-tool-helper [form-str]
+  (capture-output
+   (fn []
+     (let [form (read-string (str "(do " form-str ")"))]
+       (pr-str
+        (eval form))))))
+
+(def eval-schema
+  (json/write-str {:type :object
+                   :properties {:expression {:type :string}}
+                   :required [:expression]}))
+
+;; result json constructor
+(defn text-content [^String s]
+  (McpSchema$TextContent. s))
+
+#_(text-result "")
+
+(defn text-result [^String s]
+  (McpSchema$CallToolResult. [(McpSchema$TextContent. s)] false))
+
+(defn eval-tool-callback [exchange arguments]
+  (let [{:keys [out err result]} (eval-tool-helper (get arguments "expression"))]
+    (text-result result)))
+
+(def eval-tool
+  (McpServerFeatures$SyncToolSpecification.
+   (McpSchema$Tool. "clojure_eval" "Takes a Clojure Expression and evaluates it in the 'user namespace. For example: provide \"(+ 1 2)\" and this will evaluate that and return 3" eval-schema)
+   (reify java.util.function.BiFunction
+     (apply [this exchange arguments]
+       (eval-tool-callback exchange arguments)))))
+
+#_(eval-tool-callback nil "hello")
+
+(def hello-schema (json/write-str {:type :object}))
 
 (defn hello-tool-callback [exchange arguments]
-  (McpSchema$CallToolResult. "Hello, world!" false))
+  (McpSchema$CallToolResult. [(text-content "Hello world!") (text-content "Hello doors!") ] false))
 
 (def sync-hello-tool
   (McpServerFeatures$SyncToolSpecification.
-   (McpSchema$Tool. "hello" "Prints hello world" hello-schema)
+   (McpSchema$Tool. "hello" "Returns hello" hello-schema)
    (reify java.util.function.BiFunction
      (apply [this exchange arguments]
        (hello-tool-callback exchange arguments)))))
 
-(defn -main [& args]
+(defn mcp-server [& args]
   (let [transport-provider (StdioServerTransportProvider. (ObjectMapper.))
         server (-> (McpServer/sync transport-provider)
-                   (.serverInfo "hello-server" "0.1.0")
+                   (.serverInfo "clojure-server" "0.1.0")
                    (.capabilities (-> (McpSchema$ServerCapabilities/builder)
                                       (.tools true)
                                       (.build)))
                    (.build))]
-    (.addTool server sync-hello-tool)
-    (println "MCP Hello World Server running on STDIO transport.")
-    (while true
-      (Thread/sleep 1000))))
+    (.addTool server eval-tool)))
 
 (comment
   ;; For REPL testing:
-  (-main))
+  (-main)
+
+  )
 
