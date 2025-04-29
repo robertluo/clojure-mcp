723dada86a4fe211e15f619e17221f5a1fbf6e41
Bruce Hauman <bhauman@gmail.com>
2025-04-29 16:16:18 -0600

    getting tool conversions working

--- src/clojure_mcp/agent/langchain.clj
+++ src/clojure_mcp/agent/langchain.clj
@@ -1,6 +1,7 @@
 (ns clojure-mcp.agent.langchain
   (:require
-   [clojure.data.json :as json])
+   [clojure.data.json :as json]
+   [clojure-mcp.agent.langchain.schema :as schema])
   (:import
    ;; LangChain4j Core and Service classes
    [dev.langchain4j.service AiServices MemoryId]
@@ -57,7 +58,6 @@
         (if-let [arg-result (is-well-formed-json? arg-str)]
           (try
             (let [callback-result (promise)]
-              ;; Tool-fn expects a callback function to be called with result and error flag
               (tool-fn nil
                        (:result arg-result)
                        (fn [result error]
@@ -78,70 +78,22 @@
                   (str "ERROR: Arguments provided to the tool call were malformed.\n=====\n" arg-str "\n=====\n")
                   {})))))))
 
-(comment
-  (let [exec (registration-map->tool-executor test-tool)]
-    (.execute exec (-> (ToolExecutionRequest/builder)
-                       (.name "hello")
-                       (.arguments (json/json-str {:nm "Joey"}))
-                       (.build))
-              "asdf")))
-
-#_(registration-map->tool-executor test-tool)
-
-(defn json->JsonObjectSchema [json-str]
-  (let [schema (json/read-str json-str :key-fn keyword)
-        builder (JsonObjectSchema/builder)]
-    ;; Add properties from the schema
-    (doseq [[prop-name prop-def] (get schema :properties)]
-      (let [prop-type (get prop-def :type)
-            description (get prop-def :description "")]
-        (case prop-type
-          :string (.addStringProperty builder (name prop-name) description)
-          :integer (.addIntegerProperty builder (name prop-name) description)
-          :number (.addNumberProperty builder (name prop-name) description)
-          :boolean (.addBooleanProperty builder (name prop-name) description)
-          ;; TODO this probably needs to be recursively handled
-          ;; :object (.addObjectProperty builder (name prop-name) description)
-          :array (let [items (get prop-def :items {})
-                       item-type (get items :type)]
-                   (case item-type
-                     ;; TODO I don't think these methods exist
-                     :string (.addStringArrayProperty builder (name prop-name) description)
-                     :integer (.addIntegerArrayProperty builder (name prop-name) description)
-                     :number (.addNumberArrayProperty builder (name prop-name) description)
-                     :boolean (.addBooleanArrayProperty builder (name prop-name) description)
-                     ;; Default to string array if item type not specified or recognized
-                     (.addStringArrayProperty builder (name prop-name) description)))
-          ;; Default to string for any unrecognized types
-          (.addStringProperty builder (name prop-name) description))))
-
-    ;; Add required properties
-    (when-let [required (get schema :required)]
-      (.required builder (map name required)))
-
-    ;; Build and return the schema
-    (.build builder)))
-
 (defn registration-map->tool-specification [{:keys [name description schema]}]
+  {:pre [(string? schema) (string? name) (string? description)]}
   (-> (ToolSpecification/builder)
       (.name name)
       (.description description)
-      (.parameters (json->JsonObjectSchema schema))
+      (.parameters (schema/edn->sch
+                    (if (string? schema)
+                      (json/read-str schema :key-fn keyword)
+                      schema)))
       (.build)))
 
-#_(registration-map->tool-specification test-tool)
-
-;; This converts from a list of registration maps
-;; to a map of ToolSpecifications to ToolExecutor s
 (defn convert-tools [registration-maps]
   (into {}
         (map (juxt registration-map->tool-specification
                    registration-map->tool-executor)
              registration-maps)))
-
-#_(definterface StreamingAiService
-    (^dev.langchain4j.service.TokenStream chat [^String userMessage])
-    (^dev.langchain4j.service.TokenStream chat [^dev.langchain4j.data.message.UserMessage userMessage]))
 
 (definterface AiService
   (^String chat [^String userMessage])
@@ -155,7 +107,7 @@
          (apply [this mem-id]
            system-message)))
       (cond->
-       tools (.tools tools))
+       tools (.tools (convert-tools tools)))
       (.chatMemory memory)))
 
 (comment
@@ -169,4 +121,19 @@
                            :description "Name to say hi to"}}
                      :required [:nm]})
      :tool-fn (fn [_ {:keys [nm]} callback]
-                (callback [(str "Hello " nm "!")] false))}))
+                (callback [(str "Hello " nm "!")] false))})
+
+  (let [exec (registration-map->tool-executor test-tool)]
+    (.execute exec (-> (ToolExecutionRequest/builder)
+                       (.name "hello")
+                       (.arguments (json/json-str {:nm "Joey"}))
+                       (.build))
+              "asdf"))
+  
+  )
+
+;; keep this example
+#_(definterface StreamingAiService
+    (^dev.langchain4j.service.TokenStream chat [^String userMessage])
+    (^dev.langchain4j.service.TokenStream chat [^dev.langchain4j.data.message.UserMessage userMessage]))
+
